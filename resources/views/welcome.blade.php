7cc1f4fea4b1ff731d3a5aacc5d9cf9cca2b2e80
pvanderw <pvanderw@usc.edu>
2016-05-02 16:25:33 -0700

    fixed home screen text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome to Banger</div>
 
                 <div class="panel-body">
-                    This site works as an type of addon to soundcloud that allows soundcloud users to discover new songs based off of their preferences. Users can search for music by genre, and then filter their results by a list of factors such as number of plays, upload date, artist, etc. There is also a discover option which works like tinder, except for music. The user picks a genre, and then is shown one song at a time. They can either like the song, therefor adding it to their like tracks, or skip it causing a new song to pop up. Users can then go to their profile page and view the tracks they have liked so far and add them to their actual soundcloud profile or create playlists of their liked songs.
+                    This site works as an type of addon to soundcloud that allows soundcloud users to discover new songs based off of their preferences. Users can search for music by genre, and then filter their results by a list of factors such as genre, artist, bpm, etc. There is also a discover option which works like tinder, except for music. The user picks a genre, and then is shown one song at a time. They can either like the song, therefor adding it to their like tracks, or skip it causing a new song to pop up. Users can also add songs to their playlists and view the songs they have liked so far.
                 </div>
             </div>
         </div>
